666403c247486c4dfacbf52542c226a7ca19d475
Alex Abenoja <abenojaalex@gmail.com>
2018-10-11 10:32:45 -0600

    move cypress stuff into its own directory

--- axe-cypress/cypress/integration/axe/no-errors.spec.js
+++ axe-cypress/cypress/integration/axe/no-errors.spec.js
@@ -9,4 +9,16 @@ describe('design prototypes axe', () => {
         expect(results.violations.length).to.equal(0);
       });
   });
+
+  it('modal is error free', () => {
+    cy.visit('http://prototypes-wtw.net/containers/#modals');
+    cy.contains('Launch large modal').click();
+    cy.window().then(win => {
+      const axe = require('axe-core');
+      win.eval(axe.source);
+      return window.axe.run();
+    }).then(results => {
+      expect(results.violations.length).to.equal(0);
+    });
+  });
 });
