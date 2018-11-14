e5a41571ed215fba1e7f85654ba39abd6580c225
Alex Abenoja <abenojaalex@gmail.com>
2018-11-14 09:33:43 -0700

    Fix cypress test and migrate logic to custom commands

--- axe-cypress/cypress/integration/axe/no-errors.spec.js
+++ axe-cypress/cypress/integration/axe/no-errors.spec.js
@@ -1,24 +1,6 @@
 describe('design prototypes axe', () => {
-  it('does not have errors', () => {
-    cy.visit('https://wtw-im.github.io/es-components/')
-      .window().then(win => {
-        const axe = require('axe-core');
-        win.eval(axe.source);
-        return window.axe.run();
-      }).then(results => {
-        expect(results.violations.length).to.equal(0);
-      });
-  });
-
-  it('modal is error free', () => {
-    cy.visit('http://prototypes-wtw.net/containers/#modals');
-    cy.contains('Launch large modal').click();
-    cy.window().then(win => {
-      const axe = require('axe-core');
-      win.eval(axe.source);
-      return window.axe.run();
-    }).then(results => {
-      expect(results.violations.length).to.equal(0);
-    });
+  it('catch all the errors', () => {
+    cy.accessibilityViolations('https://alphagov.github.io/accessibility-tool-audit/test-cases.html')
+      .should('be.empty');
   });
 });
--- axe-cypress/cypress/support/commands.js
+++ axe-cypress/cypress/support/commands.js
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('checkAccessibility', (...args) => 
+  cy.visit(...args)
+    .window()
+    .then(win => {
+      const axe = require('axe-core');
+      win.eval(axe.source);
+      return win.axe.run();
+    })
+);
+
+Cypress.Commands.add('accessibilityViolations', (...args) =>
+  cy.checkAccessibility(...args)
+    .then(({ violations }) => violations)
+);
